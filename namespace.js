06c5339d13527a4648da95bca4ea6a05fced3922
Conor Scott <conorscott@Conors-MacBook-Pro.local>
2018-10-24 14:47:16 -0400

    Update namespace.js

---
+++ namespace.js
@@ -0,0 +1,32 @@
+/********************************
+Filename: namespace.js
+Author: @scot0359
+Description: namespace excercise
+Date: oct 24 2018
+*********************************/
+
+var SCOT0359 = {
+    
+    init: function(){
+        
+        let div = document.createElement("box");
+        div.className("box");
+        div.textContent("scot0359")
+        
+        let boxes = document.getElementById("box");
+        boxes.appendChild("div");
+        
+        div.addEventListener("click", clickIt);
+        div.addEventListener("mouseover", mouseOver);
+        div.addEventListener("mouseout", mouseOut);
+    }
+    
+}
+
+function clickIt(e){
+    e.style.borderColor("blue")
+    e.style.backgroundColor("lightblue")
+}
+
+function
+    
